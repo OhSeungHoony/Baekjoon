3710c5366a4fbc1bf774cecb0811eaca483bb91a
OhSeungHoony <seunghoon0702@gmail.com>
2022-01-30 13:55:43 +0900

    remove file

--- Examples/ListStack.py
+++
@@ -1,7 +0,0 @@
-class ListStack:
-    def __init__(self):
-        self.my_list = list()
-    def push(self, data):
-        self.my_list.append(data)
-    def pop(self):
-        return self.my_list.pop()
