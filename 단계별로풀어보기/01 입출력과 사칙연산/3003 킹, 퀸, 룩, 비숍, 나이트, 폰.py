3fe52d0f390aecd802187e1567fe174037a35e2c
OhSeungHoony <seunghoon0702@gmail.com>
2022-09-20 00:09:07 +0900

    .

---
+++ 단계별로풀어보기/01 입출력과 사칙연산/3003 킹, 퀸, 룩, 비숍, 나이트, 폰.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    numbers = list(map(int, input().split()))
+    chess = [1, 1, 2, 2, 2, 8]
+
+    for i in range(6):
+        print(chess[i] - numbers[i], end=" ")
---
+++ 단계별로풀어보기/03 반복문/25304 영수증.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    x = int(input())
+    n = int(input())
+    s = 0
+    for _ in range(n):
+        a, b = map(int, input().split())
+        s += a * b
+    if s == x:
+        print("Yes")
+    else:
+        print("No")
